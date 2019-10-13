abf4a3fdd2840e880116cfa300340c81036bbf85
Youssef Negm <youssefnegm@Youssefs-MacBook-Air.local>
2019-10-13 11:39:14 +0200

    test

--- index.js
+++ index.js
@@ -8,20 +8,43 @@ const app = express()
 // app.use(/* ... */)
 // app.get(/* ... */)
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  var num1 = parseInt(req.query.num1);
+  var num2 = parseInt(req.query.num2);
+  if (num2 == 0) {
+    setTimeout(function () {
+      throw new Error('We crashed!!!!!');
+    }, 10);
+    res.status(500).send()
+  }
+  var result = {
+    'result': num1 / num2
+  };
+  res.status(200).send(result);
+});
+
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
+app.use(function fourOhFourHandler(req, res) {
   res.status(404).send()
 })
-app.use(function fiveHundredHandler (err, req, res, next) {
+app.use(function fiveHundredHandler(err, req, res, next) {
   console.error(err)
   res.status(500).send()
 })
 
+
+
 // Start server
-app.listen(1234, function (err) {
+app.listen(3002, function (err) {
   if (err) {
     return console.error(err)
   }
 
-  console.log('Started at http://localhost:1234')
+  console.log('Started at http://localhost:3002')
 })
